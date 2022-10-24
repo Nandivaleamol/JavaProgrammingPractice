9eddb566125d2397813003dde229bbab271be33a
Nandivaleamol <nandivaleamol@gmail.com>
2022-10-24 14:38:06 +0530

    Bubble sorting algorithm

--- src/arrays/BubbleSorting01.java
+++ src/arrays/BubbleSorting01.java
@@ -1,4 +1,25 @@
 package arrays;
 
 public class BubbleSorting01 {
+    public static void main(String[] args) {
+        int[] a = {18,10,19,99,23};
+        int temp;
+        for (int i=0; i<a.length; i++){
+            int flag = 0;
+            for (int j=0; j<a.length-1-i; j++){
+                if (a[j]>a[j+1]){
+                    temp = a[j];
+                    a[j] =a[j+1];
+                    a[j+1] = temp;
+                    flag = 1;
+                }
+            }
+            if (flag==0){
+                break;
+            }
+        }
+        for (int i=0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
 }
