84a7485717324168c7fa99d2ad5a6a940a58ce76
Nandivaleamol <nandivaleamol@gmail.com>
2022-10-28 08:55:06 +0530

    InsertionSort

---
+++ src/arrays/InsertionSort.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] a={5,1,9,3,2,7};
+        int temp;
+        int j;
+        for (int i=1; i<a.length; i++){
+            temp = a[i];
+            j=i;
+            while (j>0 && a[j-1]>temp){
+                a[j] = a[j-1];
+                j=j-1;
+            }
+            a[j] = temp;
+        }
+        for (int i=0; i<a.length; i++){
+            System.out.print(a[i]+ " ");
+        }
+    }
+}
